f63ac13ecfa832c8dcf3be275d7737cafadbbcd2
Khushnood Asif <khushnood99@gmail.com>
2022-05-20 15:14:50 +0100

    ci-cd 2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,11 +5,15 @@
 # See https://stackoverflow.com/a/34066708.
 source /home/ec2-user/.bash_profile
 
-
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 # Goes to the directory to which CodeDeploy has copied our application files
 # (it unzips them for us as well).
 # This path is chosen by us and defined in the appspec.yml file.
 cd /var/acebook
 
 # Installs dependencies defined in package.json.
+
+
 npm install
